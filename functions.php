e140b2077b8694a701e0f5e6ed7a4eaa1419d785
AlsconWeb <alsconweb_alex@outlook.com>
2022-07-09 13:08:26 +0200

    add composer settings and functions.php

--- functions.php
+++ functions.php
@@ -1,1 +1,13 @@
 <?php
+/**
+ * Function themes.
+ *
+ * @package iwpdev/alevel.
+ */
+
+define( 'ALEVEL_VERSION', '1.0.0' );
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+new Main();
+
---
+++ src/php/Main.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Main class theme A-Level.
+ *
+ * @package iwpdev/alevel.
+ */
+
+/**
+ * Main class file.
+ */
+class Main {
+
+	/**
+	 * Main construct
+	 */
+	public function __construct() {
+		$this->init();
+	}
+
+	/**
+	 * Init hooks.
+	 *
+	 * @return void
+	 */
+	public function init(): void {
+
+	}
+
+}
